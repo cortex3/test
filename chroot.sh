bf498cec6a660009af6589210b990a9fa2ae9934
David Popp <david.popp@flyeralarm.com>
2018-04-11 10:25:54 +0200

    added packages

--- chroot.sh
+++ chroot.sh
@@ -27,16 +27,24 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 # utilities etc
 echo 'installing packages'
-pacman -S stow rxvt-unicode rofi feh compton redshift dunst sudo git base-devel lightdm lightdm-gtk-greeter zsh vim firefox xorg-server xorg-xrdb ttf-font-awesome pulseaudio maim mlocate ranger nmap --noconfirm -q
+pacman -S stow rxvt-unicode rofi feh compton redshift dunst sudo git base-devel lightdm lightdm-gtk-greeter zsh vim firefox xorg-server xorg-xrdb ttf-font-awesome pulseaudio maim mlocate ranger nmap i3-gaps networkmanager --noconfirm -q
 echo 'adding user'
 useradd -m david
 passwd david
-visudo
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 systemctl enable lightdm
+systemctl enable NetworkManager
 sudo -u david bash << EOF
 cd /home/david
 git clone https://aur.archlinux.org/trizen.git
 cd trizen
 makepkg -si
+cd /home/david
+rm -rf /home/david/trizen
+
+git clone https://gitlab.com/cortex3/dotfiles.git
+cd /home/david/dotfiles
+stow .
 EOF
+
+visudo
